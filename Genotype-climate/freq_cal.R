354e897f13096b76200abfbb95848ed8742760c3
Tang Qian <39941019+qt37t247@users.noreply.github.com>
2023-08-10 14:38:28 +0800

    R script to create inputting allele frequency
    For subsequent gradientForest

---
+++ Genotype-climate/freq_cal.R
@@ -0,0 +1,10 @@
+## Collect allele frequency files for all sampling sites
+files <- list.files(pattern = ".frq$", recursive = T)
+frqs <- do.call("cbind", lapply(files, function(fn){data.frame(Filename=fn, read.table(fn, sep = "" , header = T , na.strings ="", stringsAsFactors= F))}))
+write.csv(frqs, "raw_frequncy.csv")
+
+## Retain allele frequency only, other information convert to row and column names
+frqx <- frqs[,(1:17)*7-1]
+colnames(frqx) <- files
+rownames(frqx) <- gsub(" ", "", paste(frqs$CHR, "_", frqs$SNP))
+write.csv(t(frqx), "TT.alfreq")
