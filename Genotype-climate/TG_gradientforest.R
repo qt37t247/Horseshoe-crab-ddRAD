faafde01c7db1b550b4790d715cba85b39384705
Tang Qian <39941019+qt37t247@users.noreply.github.com>
2023-09-14 15:35:14 +0800

    Add files via upload

---
+++ Genotype-climate/TG_gradientforest.R
@@ -0,0 +1,183 @@
+library(gradientForest)
+library(raster)
+library(RColorBrewer)
+
+#Load pop allele frequency
+tg_snp <- read.csv("tg.alfreq", row.names = 1)
+#Load bio-factor
+tg_env <- read.csv("tg_env.csv", header = TRUE)
+tg_env <- tg_env[, -1]
+rownames(tg_env) <- rownames(tg_snp)#Match row names
+#GF analysis
+nSites <- dim(tg_snp)[1]
+nSpecs <- dim(tg_snp)[2]
+lev <- floor(log2(nSites*0.368/2))
+lev
+gf <- gradientForest(cbind(tg_env,tg_snp), predictor.vars=colnames(tg_env), 
+                     response.vars=colnames(tg_snp), ntree = 500, maxLevel = lev, 
+                     corr.threshold = 0.5, compact = T, nbin = 201)
+gf
+##Important variables:
+##[1] BO21_salinityrange_ss BO21_chlorange_ss     BO21_temprange_ss     BO21_temprange_bdmin 
+##[5] BO21_tempmean_ss  
+
+
+#Plot cumulative important
+most_important <- names(importance(gf))[1:19]
+plot(gf, plot.type = "O", legend = F)
+plot(gf, plot.type = "C", imp.vars = most_important, show.species = F, common.scale = T)
+
+#Simulate current genetic composition
+tg_current_bio <- read.csv("current_bio.csv")
+tg_current_bio <- tg_current_bio[,-1][complete.cases(tg_current_bio[,-1]), ]
+imp.vars <- c("BO21_salinityrange_ss","BO21_chlorange_ss","BO21_temprange_ss","BO21_temprange_bdmin","BO21_tempmean_ss")
+Trns_grid <- cbind(tg_current_bio[,c("x","y")], predict(gf,tg_current_bio[,imp.vars]))
+PCs <- prcomp(Trns_grid[,imp.vars])
+sgn <- sign(PCs$rotation["BO21_salinityrange_ss",])
+PCs$rotation <- sweep(PCs$rotation,2,sgn,"*")
+PCs$x <- sweep(PCs$x,2,sgn,"*")
+a1 <- PCs$x[,1]
+a2 <- PCs$x[,2]
+a3 <- PCs$x[,3]
+r <- a1+a2
+g <- -a2
+b <- a3+a2-a1
+r <- (r-min(r)) / (max(r)-min(r)) * 255
+g <- (g-min(g)) / (max(g)-min(g)) * 255
+b <- (b-min(b)) / (max(b)-min(b)) * 255
+
+## plotting PCA
+pdf("tg_env_PCA.pdf")
+plot(PCs$x[,c("PC1","PC2")],pch=".", ylim = c(-0.1, 0.25), cex=3, asp=1, col=rgb(r,g,b, max = 255))
+l.x <- PCs$rotation[,1]/10
+l.y <- PCs$rotation[,2]/10
+l.pos <- l.y # Create a vector of y axis coordinates
+lo <- which(l.y < 0) # Get the variables on the botgom half of the plot
+hi <- which(l.y > 0) # Get variables on the top half
+# Replace values in the vector
+l.pos <- replace(l.pos, lo, "1")
+l.pos <- replace(l.pos, hi, "3")
+text(l.x, l.y, labels=row.names(PCs$rotation), cex=0.6, pos=l.pos)
+arrows(x0=0, x1=l.x, y0=0, y1=l.y, length=0.05)
+dev.off()
+
+## plotting transformed genotype-climate association across the distribution ranges
+pdf("tg_env_PCA_sites.pdf")
+plot(Trns_grid[,c("x","y")],pch=".", cex=3, asp=1, col=rgb(r,g,b, max = 255))
+dev.off()
+
+#Simulate future genetic composition
+#Just use 2100-RCP8.5 as an example
+fut_0085 <- read.csv("fut_0085.csv")
+fut_0085 <- fut_0085[,-1][complete.cases(fut_0085[,-1]), ]
+colnames(fut_0085) <- colnames(tg_current_bio)
+Trns_grid_0085 <- cbind(fut_0085[,c("x","y")], predict(gf,fut_0085[,imp.vars]))
+##Compute the GV value
+library(dplyr)
+library(tidyr)
+euclidean <- function(a, b) sqrt(sum((a - b)^2))#Euclidean distance for genetic offset
+df <- data.frame()
+for (i in 1:nrow(fut_0085)){
+  enc_dist <- euclidean(Trns_grid[i,imp.vars], Trns_grid_0085[i,imp.vars])
+  df <- rbind(df, enc_dist)
+  names(df) <- "GV_0085"
+}
+## this for loop takes long time
+tg_gv_0085 <- cbind(fut_0085[,c("x","y")], df)
+write.csv(tg_gv_0085, "tg_genomic_offset_2100_RCP85.csv")
+
+#Convert points of average value to raster
+tg_gv_0085_raster <- rasterFromXYZ(tg_gv_0085)
+
+pdf("tg_offset_0085.pdf")
+pal <- colorRampPalette(c("yellow","red"))
+plot(tg_gv_0085_raster, col = pal(15), breaks = seq(0, 0.05, length.out=16))
+dev.off()
+
+
+
+
+#Simulate future genetic composition
+#2100-RCP2.6
+fut_0026 <- read.csv("fut_0026.csv")
+fut_0026 <- fut_0026[,-1][complete.cases(fut_0026[,-1]), ]
+colnames(fut_0026) <- colnames(tg_current_bio)
+Trns_grid_0026 <- cbind(fut_0026[,c("x","y")], predict(gf,fut_0026[,imp.vars]))
+##Compute the GV value
+library(dplyr)
+library(tidyr)
+euclidean <- function(a, b) sqrt(sum((a - b)^2))#Euclidean distance for genetic offset
+df <- data.frame()
+for (i in 1:nrow(fut_0026)){
+  enc_dist <- euclidean(Trns_grid[i,imp.vars], Trns_grid_0026[i,imp.vars])
+  df <- rbind(df, enc_dist)
+  names(df) <- "GV_0026"
+}
+## this for loop takes long time
+tg_gv_0026 <- cbind(fut_0026[,c("x","y")], df)
+write.csv(tg_gv_0026, "tg_genomic_offset_2100_RCP26.csv")
+
+#Convert points of average value to raster
+tg_gv_0026_raster <- rasterFromXYZ(tg_gv_0026)
+
+pdf("tg_offset_0026.pdf")
+pal <- colorRampPalette(c("yellow","red"))
+plot(tg_gv_0026_raster, col = pal(15), breaks = seq(0, 0.05, length.out=16))
+dev.off()
+
+
+#Simulate future genetic composition
+#2050-RCP2.6
+fut_5026 <- read.csv("fut_5026.csv")
+fut_5026 <- fut_5026[,-1][complete.cases(fut_5026[,-1]), ]
+colnames(fut_5026) <- colnames(tg_current_bio)
+Trns_grid_5026 <- cbind(fut_5026[,c("x","y")], predict(gf,fut_5026[,imp.vars]))
+##Compute the GV value
+library(dplyr)
+library(tidyr)
+euclidean <- function(a, b) sqrt(sum((a - b)^2))#Euclidean distance for genetic offset
+df <- data.frame()
+for (i in 1:nrow(fut_5026)){
+  enc_dist <- euclidean(Trns_grid[i,imp.vars], Trns_grid_5026[i,imp.vars])
+  df <- rbind(df, enc_dist)
+  names(df) <- "GV_5026"
+}
+## this for loop takes long time
+tg_gv_5026 <- cbind(fut_5026[,c("x","y")], df)
+write.csv(tg_gv_5026, "tg_genomic_offset_2050_RCP26.csv")
+
+#Convert points of average value to raster
+tg_gv_5026_raster <- rasterFromXYZ(tg_gv_5026)
+
+pdf("tg_offset_5026.pdf")
+pal <- colorRampPalette(c("yellow","red"))
+plot(tg_gv_5026_raster, col = pal(15), breaks = seq(0, 0.05, length.out=16))
+dev.off()
+
+#Simulate future genetic composition
+#2050-RCP8.5
+fut_5085 <- read.csv("fut_5085.csv")
+fut_5085 <- fut_5085[,-1][complete.cases(fut_5085[,-1]), ]
+colnames(fut_5085) <- colnames(tg_current_bio)
+Trns_grid_5085 <- cbind(fut_5085[,c("x","y")], predict(gf,fut_5085[,imp.vars]))
+##Compute the GV value
+library(dplyr)
+library(tidyr)
+euclidean <- function(a, b) sqrt(sum((a - b)^2))#Euclidean distance for genetic offset
+df <- data.frame()
+for (i in 1:nrow(fut_5085)){
+  enc_dist <- euclidean(Trns_grid[i,imp.vars], Trns_grid_5085[i,imp.vars])
+  df <- rbind(df, enc_dist)
+  names(df) <- "GV_5085"
+}
+## this for loop takes long time
+tg_gv_5085 <- cbind(fut_5085[,c("x","y")], df)
+write.csv(tg_gv_5085, "tg_genomic_offset_2050_RCP85.csv")
+
+#Convert points of average value to raster
+tg_gv_5085_raster <- rasterFromXYZ(tg_gv_5085)
+
+pdf("tg_offset_5085.pdf")
+pal <- colorRampPalette(c("yellow","red"))
+plot(tg_gv_5085_raster, col = pal(15), breaks = seq(0, 0.05, length.out=16))
+dev.off()
